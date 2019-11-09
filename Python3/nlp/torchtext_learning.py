4f1d4403e7dde0546b9f8e02f14da97a82059104
icmpnorequest <sculyt10@gmail.com>
2019-11-09 15:02:16 +0800

    Add torchtext learning

--- Python3/nlp/torchtext_learning.py
+++ Python3/nlp/torchtext_learning.py
@@ -12,6 +12,8 @@ from torchtext.vocab import Vocab
 
 dataset_path = "../../data/aclImdb/"
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 ########## get_tokenizer ##########
 tokenizer = get_tokenizer("basic_english")
@@ -38,7 +40,7 @@ print("test = {}".format(test))
 
 
 ########## BucketIterator ##########
-train_iter, test_iter = data.BucketIterator.splits((train, test), batch_sizes=(16, 256, 256), sort_key=lambda x: len(x.text), device=torch.device)
+train_iter, test_iter = data.BucketIterator.splits((train, test), batch_sizes=(16, 256, 256), sort_key=lambda x: len(x.text), device=device)
 print("train_iter = {}".format(train_iter))
 print("test_iter = {}".format(test_iter))
 
@@ -49,4 +51,10 @@ LABELS.build_vocab(train)
 
 print(vars(train[0]))
 # {'text': ['pos'], 'labels': ['Bromwell', 'High', 'is', 'a', 'cartoon', 'comedy.', 'It', 'ran', 'at', 'the', 'same', 'time', 'as', 'some', 'other', 'programs', 'about', 'school', 'life,', 'such', 'as', '"Teachers".', 'My', '35', 'years', 'in', 'the', 'teaching', 'profession', 'lead', 'me', 'to', 'believe', 'that', 'Bromwell', "High's", 'satire', 'is', 'much', 'closer', 'to', 'reality', 'than', 'is', '"Teachers".', 'The', 'scramble', 'to', 'survive', 'financially,', 'the', 'insightful', 'students', 'who', 'can', 'see', 'right', 'through', 'their', 'pathetic', "teachers'", 'pomp,', 'the', 'pettiness', 'of', 'the', 'whole', 'situation,', 'all', 'remind', 'me', 'of', 'the', 'schools', 'I', 'knew', 'and', 'their', 'students.', 'When', 'I', 'saw', 'the', 'episode', 'in', 'which', 'a', 'student', 'repeatedly', 'tried', 'to', 'burn', 'down', 'the', 'school,', 'I', 'immediately', 'recalled', '.........', 'at', '..........', 'High.', 'A', 'classic', 'line:', 'INSPECTOR:', "I'm", 'here', 'to', 'sack', 'one', 'of', 'your', 'teachers.', 'STUDENT:', 'Welcome', 'to', 'Bromwell', 'High.', 'I', 'expect', 'that', 'many', 'adults', 'of', 'my', 'age', 'think', 'that', 'Bromwell', 'High', 'is', 'far', 'fetched.', 'What', 'a', 'pity', 'that', 'it', "isn't!"]}
+
+
+######## Batch ##########
+print('Train:')
+for batch in train_iter:
+    print(batch)
 
